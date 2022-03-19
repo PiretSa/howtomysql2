3ac7726e13c109aa9650c3c8a3e6a66cec81a702
Piret Saar <piret.saar.003@khk.ee>
2022-03-19 22:37:53 +0200

    used CREATE TABLE statement

--- db.js
+++ db.js
@@ -9,10 +9,10 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    var sql = "INSERT INTO customers (name, address) VALUES ('Michelle', 'Blue Village 1')";
+    console.log("Connected!");
+    var sql = "CREATE TABLE customers (name VARCHAR(225), address VARCHAR(255))";
     con.query(sql, function (err, result) {
         if (err) throw err;
-        /*Use the result object to get the id:*/
-        console.log("1 record inserted, ID: " + result.insertId);
+        console.log("Table created");
     });
 });
